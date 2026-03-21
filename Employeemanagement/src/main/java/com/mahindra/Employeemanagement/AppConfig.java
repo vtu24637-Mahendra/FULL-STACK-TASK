f82b21594fc8a41ea21ee71526fc68dfe9cff13c
vtu24637-Mahendra <vtu24637@veltech.edu.in>
2026-03-21 21:40:29 +0530

    Delete Employeemanagement directory

--- Employeemanagement/src/main/java/com/mahindra/Employeemanagement/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.mahindra.Employeemanagement;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.mahindra.Employeemanagement")
-public class AppConfig {
-}
